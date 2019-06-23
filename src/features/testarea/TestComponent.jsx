62514db86bf595b5531a7a0f2f63f3300151212e
crslegend <risheng17@gmail.com>
2019-06-23 14:41:29 +0800

    added loading component, added login and logout logic to navbar

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,28 +1,47 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { incrementCounter, decrementCounter } from "./testActions";
+import { incrementAsync, decrementAsync } from "./testActions";
 import { Button } from "semantic-ui-react";
 import { openModal } from "../modals/modalActions";
 
 const mapStateToProps = state => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.async.loading,
+  buttonName: state.async.elementName
 });
 
 const mapDispatchToProps = {
-  incrementCounter,
-  decrementCounter,
+  incrementAsync,
+  decrementAsync,
   openModal
 };
 
 class TestComponent extends Component {
   render() {
-    const { data, incrementCounter, decrementCounter, openModal } = this.props;
+    const {
+      data,
+      incrementAsync,
+      decrementAsync,
+      openModal,
+      loading,
+      buttonName
+    } = this.props;
     return (
       <div>
         <h1>Test</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} positive />
-        <Button onClick={decrementCounter} negative />
+        <Button
+          name="increment"
+          loading={buttonName === "increment" && loading}
+          onClick={e => incrementAsync(e.target.name)}
+          positive
+        />
+        <Button
+          name="decrement"
+          loading={buttonName === "decrement" && loading}
+          onClick={e => decrementAsync(e.target.name)}
+          negative
+        />
         <Button
           onClick={() => openModal("TestModal", { data: 42 })}
           color="teal"
