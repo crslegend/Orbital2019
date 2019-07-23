a15f8119e5cdffff4571b00131abd4f264ba76bd
crslegend <risheng17@gmail.com>
2019-07-23 09:50:56 +0800

    shift edit button for event details page

--- src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -43,7 +43,7 @@ const EventDetailedHeader = ({
           <Segment basic style={eventImageTextStyle}>
             <Item.Group>
               <Grid>
-                <Grid.Column width={12}>
+                <Grid.Column width={13}>
                   <Item>
                     <Item.Content>
                       <Header
@@ -69,7 +69,7 @@ const EventDetailedHeader = ({
                     </Item.Content>
                   </Item>
                 </Grid.Column>
-                <Grid.Column width={4} verticalAlign="bottom">
+                <Grid.Column width={3} verticalAlign="bottom">
                   {isHost && (
                     <Button as={Link} to={`/manage/${event.id}`} icon inverted>
                       <Icon name="edit" />
